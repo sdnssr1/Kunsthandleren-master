57e0f026cc80b72e39bf5682e741264b3f3933d5
Saeed Nasser <90267986+sdnssr1@users.noreply.github.com>
2025-04-02 15:11:18 -0700

    update strings

--- app/src/main/java/com/example/kunsthandleren/screens/DetailScreen.kt
+++ app/src/main/java/com/example/kunsthandleren/screens/DetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.RadioButton
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
